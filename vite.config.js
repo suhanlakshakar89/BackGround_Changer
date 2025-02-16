80dc4457b08f5f637cd260e112c9846fd8e883b4
suhanlakshakar89 <suhanlakshakar@gmail.com>
2025-02-16 23:10:41 +0530

    Second Dnen

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({ 
-  base: "/My_Background_Changer/",
+  base: "/BackGround_Changer",
   plugins: [react()],
 })
